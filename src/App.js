0d83ab99eaf224a1466aaae667db3b83b2d9ee0c
unknown <vryustest1@gmail.com>
2023-02-16 16:39:38 +0900

    useRef 기본샘플

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import Timer from './componet/Timer';
+import {useState,useRef } from 'react';
+//import Timer from './componet/Timer';
 
 
 
 
 function App() {
+  
+  // const [count, setCount] = useState(0);
 
-  const [showTimer,setshowTimer] = useState(false);
+  // //useRef는 object 형태임 {current: 0}
+  // //useRef 값이 바뀌면 렌더링 되지 않지만 값은 바뀐다. 
+  // const countRef = useRef(0); 
+  // //console.log(countRef);
+
+  // const increaseCountState = () =>{
+  //   setCount(count+1);
+  // }
+
+  // const increaseCountRef = ()=>{
+  //   countRef.current++;
+  // }
+
+
+  //useRef는 다시 렌더링도 해도 값을 유지함(마운트해제될때까지)
+  //일반 변수는 렌더링시 다시 초기화 됨
+  const countRef = useRef(0);
+  var countVar = 0;
+  const [render,setRender] = useState(0);
+
+  const doRendering = ()=>{
+      setRender(render+1);
+  }
+
+  const increaseRef = ()=>{
+    countRef.current++;
+    console.log('countRef:' + countRef.current);
+  }
+
+  const increaseVar = ()=>{
+    countVar++;
+    console.log('countVar:' + countVar);
+  }
+
+
 
   return (
-    <div className="App">
-      {showTimer && <Timer /> } {/*showTimer 가 true일때만 실행*/}
-      <button onClick={()=>{ setshowTimer(!showTimer)  }}>Toggle Timer</button> {/* !showTimer 이건 현재값에 반대값이란말. showTimer 가 false면 반대값인 true, false이면 true 로 변ㄱ*/}
-     </div>
+    <div className="App">      
+    <p>ref: {countRef.current}</p>
+    <p>var: {countVar}</p>
+    <button onClick={doRendering}>랜더링하기</button>
+    <button onClick={increaseRef}>Ref올려</button>
+    <button onClick={increaseVar}>Var올려</button>
+    
+
+    </div>
   );
 }
 
